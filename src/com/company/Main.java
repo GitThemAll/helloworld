d1b5d7f51fcb796cd91e6ecba160bf5aadc83789
umarssalem <umarssalem@gmail.com>
2021-02-03 21:28:58 +0100

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int x;
+        int y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("x + y = " + (x+y));
+    }
+}

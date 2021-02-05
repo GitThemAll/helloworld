0f16d1b1b66ad0a0f60fa2da1c8d83a232cd146f
GitThemAll <47305638+GitThemAll@users.noreply.github.com>
2021-02-05 10:26:37 +0100

    Updated: add hello collaborator msg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,6 @@ public class Main {
         y = sc.nextInt();
         System.out.println("x + y = " + (x+y));
         System.out.println("x - y = " + (x-y));
+        System.out.println("helloFromCollaborator");
     }
 }
